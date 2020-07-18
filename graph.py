0b7f3100f76e90b4b3f6f48738edaeb31ca00508
Everton-Luiss <elssufpa@gmail.com>
2020-07-18 16:17:48 -0300

    Ball_wall

--- graph.py
+++ graph.py
@@ -1,5 +1,17 @@
-from visual import*
-from visual.graph import*
-f1=gdota(color=color.red)
-f1.plot(pos=(1,1))
-f1.plot(pos=(2,2))
+from vpython import* #sphere, vector, color
+
+ball = sphere(pos=vector(-5,0,0), radius=0.5, color=color.cyan)
+wallR = box(pos=vector(6,0,0), size=vector(0.2,12,12), color=color.green)
+
+ball.velocity = vector(25,0,0)
+
+deltat = 0.005
+t = 0
+ball.pos = ball.pos + ball.velocity*deltat
+
+while t < 3:
+    rate(100)
+    if ball.pos.x > wallR.pos.x:
+        ball.velocity.x = -ball.velocity.x
+    ball.pos = ball.pos + ball.velocity*deltat
+    t = t + deltat
